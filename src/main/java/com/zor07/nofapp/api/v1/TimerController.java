53a05dfc4fd5a5105928e2adeae44f7367bc10a0
zor07 <anzor.karmov@gmail.com>
2022-01-03 17:23:31 +0300

    delete timer by id

--- src/main/java/com/zor07/nofapp/api/v1/TimerController.java
+++ src/main/java/com/zor07/nofapp/api/v1/TimerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +47,11 @@ public class TimerController {
   public ResponseEntity<Void> save(@RequestBody final TimerDto timer) {
     repository.save(TimerDto.toEntity(timer));
     return new ResponseEntity<>(HttpStatus.CREATED);
+  }
+
+  @DeleteMapping("/{timerId}")
+  public ResponseEntity<Void> stopTimer(@PathVariable final Long timerId) {
+      return null;
   }
 
   @DeleteMapping("/{timerId}")
