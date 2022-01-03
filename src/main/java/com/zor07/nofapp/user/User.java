2e0658f4952ea363d1ef0a7fbf498f552f443ad9
zor07 <anzor.karmov@gmail.com>
2022-01-03 17:58:32 +0300

    fix

--- src/main/java/com/zor07/nofapp/user/User.java
+++ src/main/java/com/zor07/nofapp/user/User.java
@@ -2,13 +2,12 @@ package com.zor07.nofapp.user;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import org.hibernate.annotations.ManyToAny;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class User {
@@ -19,7 +18,7 @@ public class User {
   private String username;
   private String password;
 
-  @ManyToAny(metaColumn = @Column, fetch = FetchType.EAGER)
+  @ManyToMany(fetch = FetchType.EAGER)
   private Collection<Role> roles = new ArrayList<>();
 
   public User() {
