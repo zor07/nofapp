190121e69777a2894da3cd8472172b170bacaa53
zor07 <anzor.karmov@gmail.com>
2022-01-10 18:32:04 +0300

    refactor user service test

--- src/test/java/com/zor07/nofapp/user/UserServiceTest.java
+++ src/test/java/com/zor07/nofapp/user/UserServiceTest.java
@@ -5,16 +5,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import com.zor07.nofapp.test.AbstractUserRelatedApplicationTest;
+import com.zor07.nofapp.spring.AbstractApplicationTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class UserServiceTest extends AbstractUserRelatedApplicationTest {
+public class UserServiceTest extends AbstractApplicationTest {
+
+
+  private static final String DEFAULT_PASSWORD = "pass";
+  private static final String DEFAULT_ROLE = "role";
+
+  protected static Role createRole() {
+    return new Role(null, DEFAULT_ROLE);
+  }
+
+  protected static User createUser(final String name) {
+    return new User(null, name, name, DEFAULT_PASSWORD, new ArrayList<>());
+  }
+
 
   @Autowired
   private UserRepository userRepository;
   @Autowired
   private RoleRepository roleRepository;
+  @Autowired
+  protected UserService userService;
 
   @BeforeMethod
   void clearDb () {
@@ -37,21 +52,21 @@ public class UserServiceTest extends AbstractUserRelatedApplicationTest {
 
   @Test
   void saveRoleTest() {
-    userService.saveRole(createRole("role"));
+    userService.saveRole(createRole());
     final var user = roleRepository.findAll().get(0);
-    assertThat(user.getName()).isEqualTo("role");
+    assertThat(user.getName()).isEqualTo(DEFAULT_ROLE);
   }
 
   @Test(expectedExceptions = DataIntegrityViolationException.class)
   void saveRoleTest_failsWhenSavingRolesWithSameName() {
-    userService.saveRole(createRole("role"));
-    userService.saveRole(createRole("role"));
+    userService.saveRole(createRole());
+    userService.saveRole(createRole());
   }
 
   @Test
   void addRoleToUserTest() {
     userService.saveUser(createUser("user"));
-    userService.saveRole(createRole("role"));
+    userService.saveRole(createRole());
     userService.addRoleToUser("user", "role");
     final var user = userRepository.findByUsername("user");
     final var roles = new ArrayList<>(user.getRoles());
