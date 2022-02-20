e49b8404525d74faf2b0ed4fc313ebdeaa70fbde
zor07 <anzor.karmov@gmail.com>
2022-02-20 09:43:07 +0300

    Practice Tag Repository

---
+++ src/main/java/com/zor07/nofapp/practice/PracticeTagRepository.java
@@ -0,0 +1,6 @@
+package com.zor07.nofapp.practice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PracticeTagRepository extends JpaRepository<PracticeTag, Long> {
+}
