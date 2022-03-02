bc2ca2accb77977ff7dea387f0a59efe0ca861fb
zor07 <anzor.karmov@gmail.com>
2022-03-03 01:39:35 +0300

    find by public repo method

--- src/main/java/com/zor07/nofapp/practice/PracticeRepository.java
+++ src/main/java/com/zor07/nofapp/practice/PracticeRepository.java
@@ -2,5 +2,8 @@ package com.zor07.nofapp.practice;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PracticeRepository extends JpaRepository<Practice, Long> {
+    List<Practice> findByIsPublic(boolean isPublic);
 }
