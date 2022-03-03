dde454bb11b1615b43c831011ca63b3e2f1c6570
zor07 <anzor.karmov@gmail.com>
2022-03-03 17:56:12 +0300

    remove constructor

--- src/main/java/com/zor07/nofapp/practice/UserPractice.java
+++ src/main/java/com/zor07/nofapp/practice/UserPractice.java
@@ -35,11 +35,6 @@ public class UserPractice {
         this.practice = practice;
     }
 
-    public UserPractice(User user, Practice practice) {
-        this.user = user;
-        this.practice = practice;
-    }
-
     public UserPractice() {
     }
 
