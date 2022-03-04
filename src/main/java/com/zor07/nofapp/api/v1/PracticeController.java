c9cab2e93e5dbcb2bf3e30dae63ecbea4a12e296
zor07 <anzor.karmov@gmail.com>
2022-03-04 18:04:00 +0300

    save practice endpoint

--- src/main/java/com/zor07/nofapp/api/v1/PracticeController.java
+++ src/main/java/com/zor07/nofapp/api/v1/PracticeController.java
@@ -3,15 +3,22 @@ package com.zor07.nofapp.api.v1;
 import com.zor07.nofapp.api.v1.dto.PracticeDto;
 import com.zor07.nofapp.practice.PracticeRepository;
 import com.zor07.nofapp.practice.UserPractice;
+import com.zor07.nofapp.practice.UserPracticeKey;
 import com.zor07.nofapp.practice.UserPracticeRepository;
+import com.zor07.nofapp.security.SecurityUtils;
 import com.zor07.nofapp.user.User;
 import com.zor07.nofapp.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.transaction.Transactional;
 import java.security.Principal;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -47,6 +54,23 @@ public class PracticeController {
         return practices.stream()
                 .map(PracticeDto::toDto)
                 .toList();
+    }
+
+    @PostMapping(consumes = "application/json")
+    @Transactional
+    public ResponseEntity<Void> savePractice(@RequestBody final PracticeDto practiceDto, final Principal principal) {
+        final var user = getUser(principal);
+        if (practiceDto.isPublic) {
+            if (!SecurityUtils.isUserAdmin(user)) {
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+            }
+
+            practiceRepository.save(PracticeDto.toEntity(practiceDto));
+        } else {
+            final var practice = practiceRepository.save(PracticeDto.toEntity(practiceDto));
+            userPracticeRepository.save(new UserPractice(new UserPracticeKey(user.getId(), practice.getId()), user, practice));
+        }
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     private User getUser(final Principal principal) {
