024393648b09a2137d9c229a4868a40673bd41b8
zor07 <anzor.karmov@gmail.com>
2022-04-09 08:45:41 +0300

    Note repo

---
+++ src/main/java/com/zor07/nofapp/notebook/NoteRepository.java
@@ -0,0 +1,9 @@
+package com.zor07.nofapp.notebook;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NoteRepository extends JpaRepository<Note, Long> {
+  List<Note> findAllByNotebookId(Long notebookId);
+}
