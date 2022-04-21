c5ed14c29696fa1ac87da0027007e28f9bf0e25d
zor07 <anzor.karmov@gmail.com>
2022-04-21 19:38:04 +0300

    Note repo update

--- src/main/java/com/zor07/nofapp/notebook/NoteRepository.java
+++ src/main/java/com/zor07/nofapp/notebook/NoteRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
-  List<Note> findAllByNotebookId(Long notebookId);
+  List<NoteIdAndTitle> findAllByNotebookId(Long notebookId);
+
+  void deleteAllByIdAndNotebookId(Long id, Long notebookId);
 }
