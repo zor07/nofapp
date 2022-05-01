4d00bd2946b13d149aee0ed5148798ec6740f433
zor07 <anzor.karmov@gmail.com>
2022-05-01 23:35:05 +0300

    note mapper

---
+++ src/main/java/com/zor07/nofapp/api/v1/mapper/NoteMapper.java
@@ -0,0 +1,41 @@
+package com.zor07.nofapp.api.v1.mapper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zor07.nofapp.api.v1.dto.NoteDto;
+import com.zor07.nofapp.entity.Note;
+import com.zor07.nofapp.entity.NoteIdAndTitle;
+import com.zor07.nofapp.entity.User;
+import org.mapstruct.Context;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface NoteMapper {
+
+    ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    default String fromJsonNode(final JsonNode jsonNode) {
+        return jsonNode == null ? null : jsonNode.toString();
+    }
+
+    default JsonNode fromString(final String string) throws JsonProcessingException {
+        return string == null ? null : OBJECT_MAPPER.readTree(string);
+    }
+
+    @Mapping(target = "notebookDto", source = "notebook")
+    NoteDto toDto(final Note entity);
+
+    @Mappings({
+            @Mapping(target = "notebook", source = "notebookDto"),
+            @Mapping(target = "notebook.user", expression = "java(user)")
+    })
+    Note toEntity(final NoteDto dto, final @Context User user);
+
+    @Mapping(target = "notebookDto", ignore = true)
+    @Mapping(target = "data", ignore = true)
+    NoteDto toDto(final NoteIdAndTitle entity);
+
+}
