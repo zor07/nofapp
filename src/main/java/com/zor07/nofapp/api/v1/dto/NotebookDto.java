fbb2757acd9a3975fe2cdd2f6c67e4df4df540fb
zor07 <anzor.karmov@gmail.com>
2022-05-01 23:01:33 +0300

    removed old map methods

--- src/main/java/com/zor07/nofapp/api/v1/dto/NotebookDto.java
+++ src/main/java/com/zor07/nofapp/api/v1/dto/NotebookDto.java
@@ -1,26 +1,6 @@
 package com.zor07.nofapp.api.v1.dto;
 
-import com.zor07.nofapp.entity.Notebook;
-import com.zor07.nofapp.entity.User;
-
 public class NotebookDto {
-
-  public static NotebookDto toDto(final Notebook entity) {
-    final var dto = new NotebookDto();
-    dto.id = entity.getId();
-    dto.name = entity.getName();
-    dto.description = entity.getDescription();
-    return dto;
-  }
-
-  public static Notebook toEntity(final NotebookDto dto, User user) {
-    final var entity = new Notebook();
-    entity.setId(dto.id);
-    entity.setUser(user);
-    entity.setName(dto.name);
-    entity.setDescription(dto.description);
-    return entity;
-  }
 
   public Long id;
   public String name;
