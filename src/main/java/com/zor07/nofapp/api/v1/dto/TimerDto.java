030b42acfffa8d578838cdd8ada664ff62580870
zor07 <anzor.karmov@gmail.com>
2022-05-01 19:01:53 +0300

    removed old map methods

--- src/main/java/com/zor07/nofapp/api/v1/dto/TimerDto.java
+++ src/main/java/com/zor07/nofapp/api/v1/dto/TimerDto.java
@@ -1,33 +1,11 @@
 package com.zor07.nofapp.api.v1.dto;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import com.zor07.nofapp.entity.Timer;
-import com.zor07.nofapp.entity.User;
+
+import java.time.LocalDateTime;
 
 public class TimerDto {
-
-  public static TimerDto toDto(final Timer entity) {
-    final var timer = new TimerDto();
-    timer.id = entity.getId();
-    timer.isRunning = entity.getStop() == null;
-    timer.start = LocalDateTime.ofInstant(entity.getStart(), ZoneId.systemDefault());
-    timer.stop = entity.getStop() == null ? null : LocalDateTime.ofInstant(entity.getStop(), ZoneId.systemDefault());;
-    timer.description = entity.getDescription();
-    return timer;
-  }
-
-  public static Timer toEntity(final TimerDto dto, User user) {
-    final var timer = new Timer();
-    timer.setId(dto.id);
-    timer.setUser(user);
-    timer.setStart(dto.start.atZone(ZoneId.systemDefault()).toInstant());
-    timer.setStop(dto.stop == null ? null : dto.stop.atZone(ZoneId.systemDefault()).toInstant());
-    timer.setDescription(dto.description);
-    return timer;
-  }
 
   public Long id;
 
