14648d151876568cb7342c019983ff814b797312
zor07 <anzor.karmov@gmail.com>
2022-05-01 19:01:04 +0300

    timerMapper

---
+++ src/main/java/com/zor07/nofapp/api/v1/mapper/TimerMapper.java
@@ -0,0 +1,30 @@
+package com.zor07.nofapp.api.v1.mapper;
+
+import com.zor07.nofapp.api.v1.dto.TimerDto;
+import com.zor07.nofapp.entity.Timer;
+import com.zor07.nofapp.entity.User;
+import org.mapstruct.Context;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+
+@Mapper(componentModel = "spring")
+public interface TimerMapper {
+
+    default LocalDateTime fromInstant(Instant instant, @Context TimeZone timeZone) {
+        return instant == null ? null : LocalDateTime.ofInstant(instant, timeZone.toZoneId());
+    }
+
+    default Instant fromLocalDateTime(LocalDateTime localDateTime, @Context TimeZone timeZone) {
+        return localDateTime == null ? null : localDateTime.atZone(timeZone.toZoneId()).toInstant();
+    }
+
+    @Mapping(target = "isRunning", expression = "java(entity.getStop() == null)")
+    TimerDto toDto(final Timer entity, final @Context TimeZone timeZone);
+    @Mapping(target = "user", expression = "java(user)")
+    Timer toEntity(final TimerDto dto, final @Context TimeZone timeZone, final @Context User user);
+
+}
