5cbb50c0d1dc19f593ba181dde97a6decdb2ff8d
zor07 <anzor.karmov@gmail.com>
2022-05-02 15:15:00 +0300

    added swagger

--- src/main/java/com/zor07/nofapp/spring/ApplicationContext.java
+++ src/main/java/com/zor07/nofapp/spring/ApplicationContext.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 @Profile("!test")
@@ -16,5 +20,13 @@ public class ApplicationContext {
     return new BCryptPasswordEncoder();
   }
 
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.zor07.nofapp.api"))
+            .paths(PathSelectors.any())
+            .build();
+  }
 
 }
