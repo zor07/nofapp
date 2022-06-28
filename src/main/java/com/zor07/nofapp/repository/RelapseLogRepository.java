ea7c2eae0fcabbf1bbbe3a81e649e3590d16569b
zor07 <anzor.karmov@gmail.com>
2022-06-28 15:28:42 +0300

    RelapseLog repository

---
+++ src/main/java/com/zor07/nofapp/repository/RelapseLogRepository.java
@@ -0,0 +1,7 @@
+package com.zor07.nofapp.repository;
+
+import com.zor07.nofapp.entity.RelapseLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RelapseLogRepository extends JpaRepository<RelapseLog, Long> {
+}
