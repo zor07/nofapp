94eb24d442b9f1d56adb3c72fb1056ef933dc6ce
zor07 <anzor.karmov@gmail.com>
2022-07-01 10:03:35 +0300

    UserPostsRepository

---
+++ src/main/java/com/zor07/nofapp/repository/UserPostsRepository.java
@@ -0,0 +1,13 @@
+package com.zor07.nofapp.repository;
+
+import com.zor07.nofapp.entity.UserPost;
+import com.zor07.nofapp.entity.UserPostKey;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserPostsRepository extends JpaRepository<UserPost, UserPostKey> {
+
+    List<UserPost> findAllByUserId(Long userId);
+
+}
