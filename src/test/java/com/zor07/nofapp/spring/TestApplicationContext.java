d1bf0aa3a3510fcdf432fb409fc29cbaab7350fe
zor07 <anzor.karmov@gmail.com>
2022-07-06 17:32:34 +0300

    s3 test app context

--- src/test/java/com/zor07/nofapp/spring/TestApplicationContext.java
+++ src/test/java/com/zor07/nofapp/spring/TestApplicationContext.java
@@ -1,11 +1,15 @@
 package com.zor07.nofapp.spring;
 
 import com.zor07.nofapp.aws.s3.S3Service;
+import com.zor07.nofapp.spring.s3.S3TestContainer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 @Configuration
 @Profile("test")
@@ -17,8 +21,24 @@ public class TestApplicationContext {
   }
 
   @Bean
-  S3Service s3Service(final ApplicationConfig appConfig) {
-    return new S3Service(appConfig.getS3());
+  @ConditionalOnProperty(prefix = "config", name = "testContainers", havingValue = "true", matchIfMissing = true)
+  S3TestContainer s3TestContainer() {
+    return new S3TestContainer();
+  }
+  @Bean
+  S3Service s3Service(final ApplicationConfig config,
+                      final Optional<S3TestContainer> s3TestContainer) {
+    if (s3TestContainer.isEmpty()) {
+      return new S3Service(config.getS3());
+    } else {
+      final var cfg = new S3Service.Config();
+      cfg.setEndpoint(s3TestContainer.get().getEndpoint());
+      cfg.setAccessKey(s3TestContainer.get().getAccessKey());
+      cfg.setSecretKey(s3TestContainer.get().getSecretKey());
+      cfg.setRegion(s3TestContainer.get().getRegion());
+      cfg.setAuto(s3TestContainer.get().isAuto());
+      return new S3Service(cfg);
+    }
   }
 
 }
