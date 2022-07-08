e49eaa631552f26dffac15e149a2210239e0a4c7
zor07 <anzor.karmov@gmail.com>
2022-07-08 23:08:11 +0300

    add new method to repo

--- src/main/java/com/zor07/nofapp/repository/ProfileRepository.java
+++ src/main/java/com/zor07/nofapp/repository/ProfileRepository.java
@@ -4,4 +4,7 @@ import com.zor07.nofapp.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
+    //TODO add test
+    Profile getProfileByUserId(Long userId);
+
 }
