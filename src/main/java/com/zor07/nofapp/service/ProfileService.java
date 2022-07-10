75c8eabbfd8f919afbeac4f6e7ed13ee1271a3c8
zor07 <anzor.karmov@gmail.com>
2022-07-10 18:09:10 +0300

    update profile service

--- src/main/java/com/zor07/nofapp/service/ProfileService.java
+++ src/main/java/com/zor07/nofapp/service/ProfileService.java
@@ -1,18 +1,11 @@
 package com.zor07.nofapp.service;
 
 import com.zor07.nofapp.aws.s3.S3Service;
-import com.zor07.nofapp.entity.File;
-import com.zor07.nofapp.entity.Profile;
-import com.zor07.nofapp.entity.User;
-import com.zor07.nofapp.entity.UserPost;
-import com.zor07.nofapp.repository.FileRepository;
-import com.zor07.nofapp.repository.NoteRepository;
-import com.zor07.nofapp.repository.ProfileRepository;
-import com.zor07.nofapp.repository.UserPostsRepository;
+import com.zor07.nofapp.entity.*;
+import com.zor07.nofapp.repository.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.annotation.PostConstruct;
 import javax.transaction.Transactional;
 import java.io.IOException;
 import java.time.Instant;
@@ -23,30 +16,25 @@ public class ProfileService {
 
     private static final String USER_BUCKET = "user";
     private static final String AVATAR_KEY = "avatar";
-
     private final FileRepository fileRepository;
     private final ProfileRepository profileRepository;
     private final NoteRepository noteRepository;
     private final UserPostsRepository userPostsRepository;
+    private final RelapseLogRepository relapseLogRepository;
     private final S3Service s3;
 
     public ProfileService(FileRepository fileRepository,
                           ProfileRepository profileRepository,
                           NoteRepository noteRepository,
                           UserPostsRepository userPostsRepository,
+                          RelapseLogRepository relapseLogRepository,
                           S3Service s3) {
         this.fileRepository = fileRepository;
         this.profileRepository = profileRepository;
         this.noteRepository = noteRepository;
         this.userPostsRepository = userPostsRepository;
+        this.relapseLogRepository = relapseLogRepository;
         this.s3 = s3;
-    }
-
-    @PostConstruct
-    private void createBucket() {
-        if (!s3.containsBucket(USER_BUCKET)) {
-            s3.createBucket(USER_BUCKET);
-        }
     }
 
     public List<Profile> getProfiles() {
@@ -59,12 +47,27 @@ public class ProfileService {
 
     @Transactional
     public void saveUserAvatar(final Long userId, final MultipartFile multipartFile) throws IOException {
-        persistFile(userId, multipartFile);
-        persistFileToS3(userId, multipartFile.getBytes());
+        final var profile = profileRepository.getProfileByUserId(userId);
+        var avatar = profile.getAvatar();
+        if (avatar == null) {
+            avatar = new File();
+            avatar.setBucket(USER_BUCKET);
+            avatar.setMime(multipartFile.getContentType());
+            avatar.setPrefix(String.valueOf(userId));
+            avatar.setKey(AVATAR_KEY);
+            avatar.setSize(multipartFile.getSize());
+        } else {
+            avatar.setMime(multipartFile.getContentType());
+            avatar.setSize(multipartFile.getSize());
+        }
+        fileRepository.save(avatar);
+        persistAvatarToS3(userId, multipartFile.getBytes());
     }
 
+    @Transactional
     public void relapsed(final Long userId) {
         final var profile = profileRepository.getProfileByUserId(userId);
+        saveRelapseLog(profile.getTimerStart());
         profile.setTimerStart(Instant.now());
         profileRepository.save(profile);
     }
@@ -80,19 +83,16 @@ public class ProfileService {
         userPostsRepository.deleteUserPostByUserIdAndNoteId(userId, noteId);
     }
 
-    private void persistFile(final Long userId, final MultipartFile multipartFile) {
-        final var file = new File();
-        file.setBucket(USER_BUCKET);
-        file.setMime(multipartFile.getContentType());
-        file.setPrefix(String.valueOf(userId));
-        file.setKey(AVATAR_KEY);
-        file.setSize(multipartFile.getSize());
-        fileRepository.save(file);
-    }
-
-    private void persistFileToS3(final Long userId, final byte[] file) {
+    private void persistAvatarToS3(final Long userId, final byte[] file) {
         final var key = String.format("%s/%s", userId, AVATAR_KEY);
         s3.persistObject(USER_BUCKET, key, file);
+    }
+
+    private void saveRelapseLog(final Instant start) {
+        final var relapseLog = new RelapseLog();
+        relapseLog.setStart(start);
+        relapseLog.setStop(Instant.now());
+        relapseLogRepository.save(relapseLog);
     }
 
 }
