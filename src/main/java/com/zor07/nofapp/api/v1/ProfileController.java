c32fad1a52e9152e9bbb7d7c62901279dfff99c2
zor07 <anzor.karmov@gmail.com>
2022-07-10 18:16:35 +0300

    ProfileController draft

--- src/main/java/com/zor07/nofapp/api/v1/ProfileController.java
+++ src/main/java/com/zor07/nofapp/api/v1/ProfileController.java
@@ -1,47 +1,69 @@
 package com.zor07.nofapp.api.v1;
 
+import com.zor07.nofapp.api.v1.dto.ProfileDto;
 import com.zor07.nofapp.aws.s3.S3Service;
+import com.zor07.nofapp.service.ProfileService;
 import com.zor07.nofapp.service.UserService;
 import io.swagger.annotations.Api;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.security.Principal;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/profile/{userId}")
+@RequestMapping("/api/v1/profiles")
 @Api( tags = "Profile" )
 public class ProfileController {
 
     private final S3Service s3Service;
     private final UserService userService;
 
+    private final ProfileService profileService;
+
     public ProfileController(final S3Service s3Service,
-                             final UserService userService) {
+                             final UserService userService,
+                             final ProfileService profileService) {
         this.s3Service = s3Service;
         this.userService = userService;
+        this.profileService = profileService;
     }
 
-    @PostMapping("/avatar")
+    @GetMapping
+    public ResponseEntity<List<ProfileDto>> getProfiles(final Principal principal) {
+        throw new UnsupportedOperationException();
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<ProfileDto> getProfile(final Principal principal,
+                                                 final @PathVariable Long userId) {
+        throw new UnsupportedOperationException();
+    }
+
+    @PostMapping("/{userId}/avatar")
     public ResponseEntity<Void> uploadAvatar(final Principal principal,
-                                             final @RequestParam("file") MultipartFile file) throws IOException {
-        final var user = userService.getUser(principal);
+                                             final @PathVariable Long userId,
+                                             final @RequestParam("file") MultipartFile file){
+        throw new UnsupportedOperationException();
+    }
 
-        final var bucket = "user";
+    @DeleteMapping("/{userId}/avatar")
+    public ResponseEntity<Void> deleteAvatar(final Principal principal) {
+        throw new UnsupportedOperationException();
+    }
 
+    @PostMapping("/{userId}/relapsed")
+    public ResponseEntity<ProfileDto> relapsed(final Principal principal,
+                                               final @PathVariable Long userId) {
 
-//        s3Service.createBucket("user");
-        if (!s3Service.containsBucket(bucket)) {
-            s3Service.createBucket(bucket);
-        }
+        throw new UnsupportedOperationException();
+    }
 
-        s3Service.persistObject(bucket, String.format("%s/avatar", user.getId()), file.getBytes());
-        return ResponseEntity.ok().build();
+    @PostMapping("/{userId}/posts/{noteId}")
+    public ResponseEntity<ProfileDto> addPostToProfile(final Principal principal,
+                                                       final @PathVariable Long userId) {
+        throw new UnsupportedOperationException();
     }
 
 }
