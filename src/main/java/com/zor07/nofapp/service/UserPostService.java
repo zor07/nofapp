2d4fa5c492a50779f65b6a7aab85504f467a6446
zor07 <anzor.karmov@gmail.com>
2022-07-13 22:35:35 +0300

    UserPostService

---
+++ src/main/java/com/zor07/nofapp/service/UserPostService.java
@@ -0,0 +1,35 @@
+package com.zor07.nofapp.service;
+
+import com.zor07.nofapp.entity.User;
+import com.zor07.nofapp.entity.UserPost;
+import com.zor07.nofapp.repository.NoteRepository;
+import com.zor07.nofapp.repository.UserPostsRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class UserPostService {
+
+    private final UserPostsRepository userPostsRepository;
+    private final NoteRepository noteRepository;
+
+    public UserPostService(final UserPostsRepository userPostsRepository,
+                           final NoteRepository noteRepository) {
+        this.userPostsRepository = userPostsRepository;
+        this.noteRepository = noteRepository;
+    }
+
+    public void addPostToUser(final User user, final Long noteId) {
+        final var post = new UserPost();
+        post.setUser(user);
+        post.setNote(noteRepository.getById(noteId));
+        userPostsRepository.save(post);
+    }
+
+    @Transactional
+    public void removePostFromUser(final Long userId, final Long noteId){
+        userPostsRepository.deleteUserPostByUserIdAndNoteId(userId, noteId);
+    }
+
+}
