e25e4358912116525b16153f9bd190891da4b9d6
zor07 <anzor.karmov@gmail.com>
2022-07-13 23:24:52 +0300

    getUserPosts endpoint

--- src/main/java/com/zor07/nofapp/api/v1/UserPostsController.java
+++ src/main/java/com/zor07/nofapp/api/v1/UserPostsController.java
@@ -1,18 +1,22 @@
 package com.zor07.nofapp.api.v1;
 
+import com.zor07.nofapp.api.v1.dto.NoteDto;
 import com.zor07.nofapp.api.v1.dto.ProfileDto;
+import com.zor07.nofapp.api.v1.mapper.NoteMapper;
 import com.zor07.nofapp.service.UserPostService;
 import com.zor07.nofapp.service.UserService;
 import io.swagger.annotations.Api;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.List;
 import java.util.Objects;
 
 @RestController
@@ -24,11 +28,26 @@ public class UserPostsController {
 
     private final UserPostService userPostService;
 
+    private final NoteMapper noteMapper;
+
     public UserPostsController(final UserService userService,
-                               final UserPostService userPostService) {
+                               final UserPostService userPostService,
+                               final NoteMapper noteMapper) {
         this.userService = userService;
         this.userPostService = userPostService;
+        this.noteMapper = noteMapper;
     }
+
+    @GetMapping
+    public ResponseEntity<List<NoteDto>> getUserPosts(final @PathVariable Long userId) {
+        final var notes = userPostService.getUserPosts(userId)
+                .stream()
+                .map(noteMapper::toDto)
+                .toList();
+
+        return ResponseEntity.ok(notes);
+    }
+
 
     @PostMapping("/{noteId}")
     public ResponseEntity<ProfileDto> addPostToProfile(final Principal principal,
