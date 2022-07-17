0a89293e5aab27853051e14a599e5b9bbb9bd4fa
zor07 <anzor.karmov@gmail.com>
2022-07-17 16:36:45 +0300

    update profile dto

--- src/main/java/com/zor07/nofapp/api/v1/dto/ProfileDto.java
+++ src/main/java/com/zor07/nofapp/api/v1/dto/ProfileDto.java
@@ -2,11 +2,12 @@ package com.zor07.nofapp.api.v1.dto;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.zor07.nofapp.api.v1.dto.auth.UserInfoDto;
 
 import java.time.LocalDateTime;
 
 public record ProfileDto(
         Long id,
-        Long userId,
+        UserInfoDto user,
         String avatarUri,
         @JsonSerialize(using = LocalDateTimeSerializer.class) LocalDateTime timerStart) {}
