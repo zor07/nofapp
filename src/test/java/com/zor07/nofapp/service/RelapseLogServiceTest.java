6e1221bcfe4b7bfc94fff269a7713e1770d9a56f
zor07 <anzor.karmov@gmail.com>
2022-07-30 18:44:00 +0300

    relapseLog service test

---
+++ src/test/java/com/zor07/nofapp/service/RelapseLogServiceTest.java
@@ -0,0 +1,95 @@
+package com.zor07.nofapp.service;
+
+import com.zor07.nofapp.entity.RelapseLog;
+import com.zor07.nofapp.entity.User;
+import com.zor07.nofapp.repository.RelapseLogRepository;
+import com.zor07.nofapp.repository.UserRepository;
+import com.zor07.nofapp.spring.AbstractApplicationTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Instant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RelapseLogServiceTest extends AbstractApplicationTest {
+
+    private static final String USERNAME = "user";
+    private static final String PASSWORD = "pass";
+
+    private static final Instant START_1 = Instant.parse("2022-05-01T15:26:00Z");
+    private static final Instant STOP_1 = Instant.parse("2022-05-01T15:27:00Z");
+
+    @Autowired
+    private RelapseLogRepository relapseLogRepository;
+    @Autowired
+    private RelapseLogService relapseLogService;
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @BeforeMethod
+    @AfterClass
+    void clearDb() {
+        relapseLogRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
+    private User persistUser() {
+        final var user = new User();
+        user.setName(USERNAME);
+        user.setUsername(USERNAME);
+        user.setPassword(PASSWORD);
+        return userRepository.save(user);
+    }
+
+    @Test
+    void findAllByUserIdTest() {
+        // given
+        final var user = persistUser();
+        final var all = relapseLogRepository.findAll();
+        assertThat(all).isEmpty();
+        persistRelapseLog(createRelapseLog(user));
+        persistRelapseLog(createRelapseLog(user));
+        persistRelapseLog(createRelapseLog(user));
+
+        // when
+        final var logs = relapseLogService.getRelapseLogEntriesByUserId(user.getId());
+
+        // then
+        assertThat(logs).hasSize(3);
+        assertThat(logs.get(0).getUser().getId()).isEqualTo(user.getId());
+        assertThat(logs.get(0).getStart()).isEqualTo(START_1);
+        assertThat(logs.get(0).getStop()).isEqualTo(STOP_1);
+    }
+
+    @Test
+    void deleteByIdAndUserIdTest() {
+        // given
+        assertThat(relapseLogRepository.findAll()).isEmpty();
+        final var user = persistUser();
+        persistRelapseLog(createRelapseLog(user));
+        final var id = relapseLogRepository.findAll().get(0).getId();
+
+        // when
+        relapseLogService.deleteRelapseLog(id, user.getId());
+
+        // then
+        assertThat(relapseLogRepository.findAll()).isEmpty();
+
+    }
+
+    private RelapseLog createRelapseLog(final User user) {
+        final var log = new RelapseLog();
+        log.setUser(user);
+        log.setStart(START_1);
+        log.setStop(STOP_1);
+        return log;
+    }
+
+    private RelapseLog persistRelapseLog(final RelapseLog log) {
+        return relapseLogRepository.save(log);
+    }
+}
