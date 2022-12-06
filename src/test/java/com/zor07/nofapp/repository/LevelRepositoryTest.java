b909d0cb25fb2821e528fdd51ba634b9cb2efc53
zor07 <anzor.karmov@gmail.com>
2022-12-06 19:56:15 +0300

    Level repository test refactor

--- src/test/java/com/zor07/nofapp/repository/LevelRepositoryTest.java
+++ src/test/java/com/zor07/nofapp/repository/LevelRepositoryTest.java
@@ -1,7 +1,7 @@
 package com.zor07.nofapp.repository;
 
-import com.zor07.nofapp.entity.levels.Level;
 import com.zor07.nofapp.spring.AbstractApplicationTest;
+import com.zor07.nofapp.test.LevelTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
@@ -11,10 +11,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LevelRepositoryTest extends AbstractApplicationTest {
 
-    private static final Integer ORDER = 1;
-    private static final Integer NEW_ORDER = 2;
-    private static final String NAME = "name";
-    private static final String NEW_NAME = "new name";
     @Autowired
     private LevelRepository levelRepository;
 
@@ -26,28 +22,21 @@ public class LevelRepositoryTest extends AbstractApplicationTest {
 
     @Test
     void testCrud() {
-
         levelRepository.deleteAll();
         final var all = levelRepository.findAll();
         assertThat(all).isEmpty();
 
-        final var level = new Level();
-        level.setName(NAME);
-        level.setOrder(ORDER);
+        final var level = LevelTestUtils.getBlankEntity();
 
         final var id = levelRepository.save(level).getId();
         final var inserted = levelRepository.findById(id).get();
-        assertThat(inserted).isNotNull();
-        assertThat(inserted.getName()).isEqualTo(NAME);
-        assertThat(inserted.getOrder()).isEqualTo(ORDER);
+        LevelTestUtils.checkEntity(level, inserted, false);
+        LevelTestUtils.updateEntity(inserted);
 
-        inserted.setName(NEW_NAME);
-        inserted.setOrder(NEW_ORDER);
         levelRepository.save(inserted);
 
         final var updated = levelRepository.findById(id).get();
-        assertThat(updated.getName()).isEqualTo(NEW_NAME);
-        assertThat(inserted.getOrder()).isEqualTo(NEW_ORDER);
+        LevelTestUtils.checkUpdated(inserted);
 
         levelRepository.delete(updated);
 
