05eadc5920314ebc4315a02230b78acc988461a6
zor07 <anzor.karmov@gmail.com>
2022-12-06 17:28:12 +0300

    renamed app

--- src/main/java/com/zor07/nofapp/NFApp.java
+++ src/main/java/com/zor07/nofapp/NFApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NofappApplication {
+public class NFApp {
 
   public static void main(String[] args) {
-    SpringApplication.run(NofappApplication.class, args);
+    SpringApplication.run(NFApp.class, args);
   }
 
 }
