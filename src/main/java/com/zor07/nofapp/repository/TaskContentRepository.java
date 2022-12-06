4c093a4899fb9252c82cdad1e6f464ee173b2f16
zor07 <anzor.karmov@gmail.com>
2022-12-06 19:25:15 +0300

    TaskContent repository

---
+++ src/main/java/com/zor07/nofapp/repository/TaskContentRepository.java
@@ -0,0 +1,9 @@
+package com.zor07.nofapp.repository;
+
+import com.zor07.nofapp.entity.levels.TaskContent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskContentRepository extends JpaRepository<TaskContent, Long> {
+}
+
+
