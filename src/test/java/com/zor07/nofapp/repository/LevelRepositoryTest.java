b3c72943f0f821f6dc1108b52eea89202b89dfc7
zor07 <anzor.karmov@gmail.com>
2022-12-06 17:42:51 +0300

    updated level repository test

--- src/test/java/com/zor07/nofapp/repository/LevelRepositoryTest.java
+++ src/test/java/com/zor07/nofapp/repository/LevelRepositoryTest.java
@@ -11,6 +11,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LevelRepositoryTest extends AbstractApplicationTest {
 
+    private static final Integer ORDER = 1;
+    private static final Integer NEW_ORDER = 2;
     private static final String NAME = "name";
     private static final String NEW_NAME = "new name";
     @Autowired
@@ -31,17 +33,21 @@ public class LevelRepositoryTest extends AbstractApplicationTest {
 
         final var level = new Level();
         level.setName(NAME);
+        level.setOrder(ORDER);
 
         final var id = levelRepository.save(level).getId();
         final var inserted = levelRepository.findById(id).get();
         assertThat(inserted).isNotNull();
         assertThat(inserted.getName()).isEqualTo(NAME);
+        assertThat(inserted.getOrder()).isEqualTo(ORDER);
 
         inserted.setName(NEW_NAME);
+        inserted.setOrder(NEW_ORDER);
         levelRepository.save(inserted);
 
         final var updated = levelRepository.findById(id).get();
         assertThat(updated.getName()).isEqualTo(NEW_NAME);
+        assertThat(inserted.getOrder()).isEqualTo(NEW_ORDER);
 
         levelRepository.delete(updated);
 
