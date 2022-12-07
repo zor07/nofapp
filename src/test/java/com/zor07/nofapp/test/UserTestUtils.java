b8e5392f898c8bac36589862f90f03ff085c2cc1
zor07 <anzor.karmov@gmail.com>
2022-12-07 16:32:46 +0300

    UserTestUtils

---
+++ src/test/java/com/zor07/nofapp/test/UserTestUtils.java
@@ -0,0 +1,52 @@
+package com.zor07.nofapp.test;
+
+import com.zor07.nofapp.entity.Role;
+import com.zor07.nofapp.entity.User;
+import com.zor07.nofapp.entity.levels.Task;
+import com.zor07.nofapp.security.UserRole;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserTestUtils {
+
+    public static final String DEFAULT_ROLE = "ROLE_USER";
+    public static final String DEFAULT_USERNAME = "user";
+    public static final String DEFAULT_PASSWORD = "pass";
+
+    public static Role createRole() {
+        return new Role(null, DEFAULT_ROLE);
+    }
+
+    public static Role createAdminRole() {
+        return new Role(null, UserRole.ROLE_ADMIN.getRoleName());
+    }
+
+    public static User createUser() {
+        return createUser(DEFAULT_USERNAME);
+    }
+
+    public static User createUser(final String name) {
+        return new User(null, name, name, DEFAULT_PASSWORD, new ArrayList<>());
+    }
+
+    public static void checkEntity(
+            final User actual,
+            final User expected,
+            final boolean checkId
+    ) {
+        if (checkId) {
+            assertThat(actual.getId()).isEqualTo(expected.getId());
+        }
+        assertThat(actual.getName()).isEqualTo(expected.getName());
+        assertThat(actual.getUsername()).isEqualTo(expected.getUsername());
+        assertThat(actual.getPassword()).isEqualTo(expected.getPassword());
+        assertThat(actual.getRoles()).allMatch(
+                role -> role.getName().equals(DEFAULT_ROLE)
+        );
+    }
+
+}
