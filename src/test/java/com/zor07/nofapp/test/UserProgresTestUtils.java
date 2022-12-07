67935550747eeb64913ce5552008a05f0bd8fd3a
zor07 <anzor.karmov@gmail.com>
2022-12-07 16:32:57 +0300

    UserProgresTestUtils

---
+++ src/test/java/com/zor07/nofapp/test/UserProgresTestUtils.java
@@ -0,0 +1,34 @@
+package com.zor07.nofapp.test;
+
+import com.zor07.nofapp.entity.User;
+import com.zor07.nofapp.entity.UserProgress;
+import com.zor07.nofapp.entity.levels.Task;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserProgresTestUtils {
+
+    public static UserProgress getBlankEntity(final User user, final Task task) {
+        final var userProgress = new UserProgress();
+        userProgress.setUser(user);
+        userProgress.setCurrentTask(task);
+        return userProgress;
+    }
+
+    public static void checkUpdated(final UserProgress userProgress) {
+        TaskTestUtils.checkUpdated(userProgress.getCurrentTask());
+    }
+
+    public static void checkEntity(
+            final UserProgress actual,
+            final UserProgress expected,
+            final boolean checkId
+    ) {
+        if (checkId) {
+            assertThat(actual.getId()).isEqualTo(expected.getId());
+        }
+        UserTestUtils.checkEntity(actual.getUser(), expected.getUser(), checkId);
+        TaskTestUtils.checkEntity(actual.getCurrentTask(), expected.getCurrentTask(), checkId);
+    }
+
+}
