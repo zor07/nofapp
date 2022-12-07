55935829a9e5ce9a92d9da17b9544c7c788c11ab
zor07 <anzor.karmov@gmail.com>
2022-12-07 16:32:20 +0300

    UserProgress entity

---
+++ src/main/java/com/zor07/nofapp/entity/UserProgress.java
@@ -0,0 +1,64 @@
+package com.zor07.nofapp.entity;
+
+import com.zor07.nofapp.entity.levels.Task;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_progress", schema = "public")
+public class UserProgress {
+
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_progress_id_seq"
+    )
+    @SequenceGenerator(
+            name = "user_progress_id_seq",
+            sequenceName = "user_progress_id_seq",
+            allocationSize = 1
+    )
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "current_task_id", referencedColumnName = "id")
+    private Task currentTask;
+
+    public UserProgress(Long id, User user, Task currentTask) {
+        this.id = id;
+        this.user = user;
+        this.currentTask = currentTask;
+    }
+
+    public UserProgress() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Task getCurrentTask() {
+        return currentTask;
+    }
+
+    public void setCurrentTask(Task currentTask) {
+        this.currentTask = currentTask;
+    }
+}
