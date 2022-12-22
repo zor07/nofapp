1dd5f7d87215474ec3ed37ead361baf3b86c4b42
zor07 <anzor.karmov@gmail.com>
2022-12-22 17:51:36 +0300

    infrastructure test draft

---
+++ infrastructure/src/test/java/com/zor07/nofapp/infrastructure/file/repository/FileRepositoryTest.java
@@ -0,0 +1,79 @@
+//package com.zor07.nofapp.infrastructure.file.repository;//package com.zor07.nofapp.infrastructure.repository.file.repository;
+//
+//import com.zor07.nofapp.file.entity.FileEntity;
+//import com.zor07.nofapp.file.repository.FileRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.testng.annotations.AfterClass;
+//import org.testng.annotations.BeforeMethod;
+//import org.testng.annotations.Test;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//public class FileRepositoryTest extends AbstractApplicationTest {
+//
+//    private static final String BUCKET_1 = "bucket_1";
+//    private static final String PREFIX_1 = "prefix_1";
+//    private static final String KEY_1 = "key_1";
+//    private static final String MIME_1 = "mime_1";
+//    private static final long SIZE_1 = 1L;
+//    private static final String BUCKET_2 = "bucket_2";
+//    private static final String PREFIX_2 = "prefix_2";
+//    private static final String KEY_2 = "key_2";
+//    private static final String MIME_2 = "mime_2";
+//    private static final long SIZE_2 = 2L;
+//
+//
+//    @Autowired
+//    private FileRepository fileRepository;
+//
+//
+//    @BeforeMethod
+//    @AfterClass
+//    void clearDb() {
+//        fileRepository.deleteAll();
+//    }
+//
+//    @Test
+//    void testCrud() {
+//
+//        fileRepository.deleteAll();
+//        final var all = fileRepository.findAll();
+//        assertThat(all).isEmpty();
+//
+//        final var file = new FileEntity();
+//        file.setBucket(BUCKET_1);
+//        file.setPrefix(PREFIX_1);
+//        file.setKey(KEY_1);
+//        file.setMime(MIME_1);
+//        file.setSize(SIZE_1);
+//
+//        final var id = fileRepository.save(file).getId();
+//        final var inserted = fileRepository.findById(id).get();
+//        assertThat(inserted).isNotNull();
+//        assertThat(inserted.getBucket()).isEqualTo(BUCKET_1);
+//        assertThat(inserted.getPrefix()).isEqualTo(PREFIX_1);
+//        assertThat(inserted.getKey()).isEqualTo(KEY_1);
+//        assertThat(inserted.getMime()).isEqualTo(MIME_1);
+//        assertThat(inserted.getSize()).isEqualTo(SIZE_1);
+//
+//        inserted.setBucket(BUCKET_2);
+//        inserted.setPrefix(PREFIX_2);
+//        inserted.setKey(KEY_2);
+//        inserted.setMime(MIME_2);
+//        inserted.setSize(SIZE_2);
+//        fileRepository.save(inserted);
+//
+//        final var updated = fileRepository.findById(id).get();
+//        assertThat(updated).isNotNull();
+//        assertThat(updated.getBucket()).isEqualTo(BUCKET_2);
+//        assertThat(updated.getPrefix()).isEqualTo(PREFIX_2);
+//        assertThat(updated.getKey()).isEqualTo(KEY_2);
+//        assertThat(updated.getMime()).isEqualTo(MIME_2);
+//        assertThat(updated.getSize()).isEqualTo(SIZE_2);
+//
+//        fileRepository.delete(updated);
+//
+//        assertThat(fileRepository.findById(id)).isEmpty();
+//    }
+//
+//}
---
+++ infrastructure/src/test/java/com/zor07/nofapp/infrastructure/levels/repository/LevelRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.zor07.nofapp.infrastructure.levels.repository;//package com.zor07.nofapp.infrastructure.repository.levels.repository;
+//
+//import com.zor07.nofapp.levels.repository.LevelRepository;
+//import com.zor07.nofapp.spring.AbstractApplicationTest;
+//import com.zor07.nofapp.test.LevelTestUtils;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.testng.annotations.AfterTest;
+//import org.testng.annotations.BeforeClass;
+//import org.testng.annotations.Test;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//public class LevelRepositoryTest extends AbstractApplicationTest {
+//
+//    @Autowired
+//    private LevelRepository levelRepository;
+//
+//    @BeforeClass
+//    @AfterTest
+//    void clearDb() {
+//        levelRepository.deleteAll();
+//    }
+//
+//    @Test
+//    void testCrud() {
+//        levelRepository.deleteAll();
+//        final var all = levelRepository.findAll();
+//        assertThat(all).isEmpty();
+//
+//        final var level = LevelTestUtils.getBlankEntity();
+//
+//        final var id = levelRepository.save(level).getId();
+//        final var inserted = levelRepository.findById(id).get();
+//        LevelTestUtils.checkEntity(level, inserted, false);
+//        LevelTestUtils.updateEntity(inserted);
+//
+//        levelRepository.save(inserted);
+//
+//        final var updated = levelRepository.findById(id).get();
+//        LevelTestUtils.checkUpdated(inserted);
+//
+//        levelRepository.delete(updated);
+//
+//        assertThat(levelRepository.findById(id)).isEmpty();
+//    }
+//}
