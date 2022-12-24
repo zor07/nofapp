87b5badd0b16a67d489236bd79d2f69c72c89738
zor07 <anzor.karmov@gmail.com>
2022-12-24 15:33:25 +0300

    task findAllByLevelId

--- src/main/java/com/zor07/nofapp/repository/levels/TaskRepository.java
+++ src/main/java/com/zor07/nofapp/repository/levels/TaskRepository.java
@@ -3,5 +3,10 @@ package com.zor07.nofapp.repository.levels;
 import com.zor07.nofapp.entity.levels.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findAllByLevelId(Long levelId);
+
 }
