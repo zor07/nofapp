4e9fb6bd41e74bf365bdf92646a33309fa673fdc
zor07 <anzor.karmov@gmail.com>
2022-12-24 17:30:28 +0300

    task content toDto mapper test

---
+++ src/test/java/com/zor07/nofapp/api/v1/dto/level/mapper/TaskContentMapperTest.java
@@ -0,0 +1,31 @@
+package com.zor07.nofapp.api.v1.dto.level.mapper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zor07.nofapp.test.FileTestUtils;
+import com.zor07.nofapp.test.TaskContentTestUtils;
+import org.mapstruct.factory.Mappers;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TaskContentMapperTest {
+
+    private final TaskContentMapper mapper = Mappers.getMapper(TaskContentMapper.class);
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private static final Long ID = 1L;
+
+    @Test
+    void toDtoTest() throws JsonProcessingException {
+        final var file = FileTestUtils.getBlankEntity();
+        final var entity = TaskContentTestUtils.getBlankEntity(ID, file);
+
+        final var dto = mapper.toDto(entity);
+
+        assertThat(dto.id()).isEqualTo(entity.getId());
+        assertThat(dto.title()).isEqualTo(entity.getTitle());
+        assertThat(dto.fileUri()).isEqualTo(String.format("%s/%s", entity.getFile().getBucket(), entity.getFile().getKey()));
+        assertThat(dto.data()).isEqualTo(objectMapper.readTree(entity.getData()));
+    }
+
+}
