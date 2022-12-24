826cbc54823a1aa81f19a86ddfa3d162489c8a94
zor07 <anzor.karmov@gmail.com>
2022-12-24 15:41:56 +0300

    moved file

--- src/main/java/com/zor07/nofapp/api/v1/controller/PracticeController.java
+++ src/main/java/com/zor07/nofapp/api/v1/controller/PracticeController.java
@@ -1,4 +1,4 @@
-package com.zor07.nofapp.api.v1;
+package com.zor07.nofapp.api.v1.controller;
 
 import com.zor07.nofapp.api.v1.dto.practice.PracticeDto;
 import com.zor07.nofapp.api.v1.dto.practice.mapper.PracticeMapper;
