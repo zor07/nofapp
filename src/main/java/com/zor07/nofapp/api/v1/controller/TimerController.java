e5a2d616f9f648a3e01c8ad96d07f86e8d91d702
zor07 <anzor.karmov@gmail.com>
2022-12-24 15:42:31 +0300

    moved file

--- src/main/java/com/zor07/nofapp/api/v1/controller/TimerController.java
+++ src/main/java/com/zor07/nofapp/api/v1/controller/TimerController.java
@@ -1,4 +1,4 @@
-package com.zor07.nofapp.api.v1;
+package com.zor07.nofapp.api.v1.controller;
 
 import com.zor07.nofapp.api.v1.dto.timer.TimerDto;
 import com.zor07.nofapp.api.v1.dto.timer.mapper.TimerMapper;
