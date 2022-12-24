b630fa96af8516c377b226c8d631287c04e456e9
zor07 <anzor.karmov@gmail.com>
2022-12-24 16:06:13 +0300

    moved file

--- src/test/java/com/zor07/nofapp/api/v1/controller/NotebookControllerTest.java
+++ src/test/java/com/zor07/nofapp/api/v1/controller/NotebookControllerTest.java
@@ -1,4 +1,4 @@
-package com.zor07.nofapp.api.v1;
+package com.zor07.nofapp.api.v1.controller;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.zor07.nofapp.entity.notes.Notebook;
