a29f0a09c84ecd287a87294d73a864d958ece3e0
zor07 <anzor.karmov@gmail.com>
2022-12-26 19:43:19 +0300

    task service

---
+++ src/main/java/com/zor07/nofapp/service/levels/TaskService.java
@@ -0,0 +1,10 @@
+package com.zor07.nofapp.service.levels;
+
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class TaskService {
+}
