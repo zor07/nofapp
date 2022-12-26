b730fcca1f9a006ddefd8e988dc1cfccb21ec81b
zor07 <anzor.karmov@gmail.com>
2022-12-26 19:48:51 +0300

    task service methods

--- src/main/java/com/zor07/nofapp/service/levels/TaskService.java
+++ src/main/java/com/zor07/nofapp/service/levels/TaskService.java
@@ -1,10 +1,16 @@
 package com.zor07.nofapp.service.levels;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.zor07.nofapp.entity.file.File;
+import com.zor07.nofapp.entity.level.Task;
+import com.zor07.nofapp.entity.level.TaskContent;
 import com.zor07.nofapp.repository.level.TaskContentRepository;
 import com.zor07.nofapp.repository.level.TaskRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 @Transactional
@@ -17,5 +23,34 @@ public class TaskService {
                        final TaskContentRepository taskContentRepository) {
         this.taskRepository = taskRepository;
         this.taskContentRepository = taskContentRepository;
+    }
+
+
+    public List<Task> getAllByLevelId(Long levelId) {
+        return Collections.emptyList();
+    }
+
+    public Task save(Task task) {
+        return null;
+    }
+
+    public void delete(Long id) {
+
+    }
+
+    public void deleteTaskContent(Long taskContentId) {
+
+    }
+
+    public Task createTaskContent(TaskContent content) {
+        return null;
+    }
+
+    public Task addVideoToTask(Long taskId, File file) {
+        return null;
+    }
+
+    public Task addTextToTask(Long taskId, JsonNode jsonNode) {
+        return null;
     }
 }
