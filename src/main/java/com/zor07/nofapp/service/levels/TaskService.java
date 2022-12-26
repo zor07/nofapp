49d41fc7806cd0bf28a4f722088e824e1ee36470
zor07 <anzor.karmov@gmail.com>
2022-12-26 19:44:30 +0300

    task service dependencies

--- src/main/java/com/zor07/nofapp/service/levels/TaskService.java
+++ src/main/java/com/zor07/nofapp/service/levels/TaskService.java
@@ -1,5 +1,7 @@
 package com.zor07.nofapp.service.levels;
 
+import com.zor07.nofapp.repository.level.TaskContentRepository;
+import com.zor07.nofapp.repository.level.TaskRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -7,4 +9,13 @@ import javax.transaction.Transactional;
 @Service
 @Transactional
 public class TaskService {
+
+    private final TaskRepository taskRepository;
+    private final TaskContentRepository taskContentRepository;
+
+    public TaskService(final TaskRepository taskRepository,
+                       final TaskContentRepository taskContentRepository) {
+        this.taskRepository = taskRepository;
+        this.taskContentRepository = taskContentRepository;
+    }
 }
