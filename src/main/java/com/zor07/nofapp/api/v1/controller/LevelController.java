976f7edd9c0150769a4134421d8e6bed79123e93
zor07 <anzor.karmov@gmail.com>
2022-12-26 15:42:45 +0300

    level controller

---
+++ src/main/java/com/zor07/nofapp/api/v1/controller/LevelController.java
@@ -0,0 +1,15 @@
+package com.zor07.nofapp.api.v1.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api/v1/levels")
+@Api(tags = "Level")
+public class LevelController {
+
+
+
+
+}
