d571ea6612e4ad4b90417fa7f64a3dea51d64b5c
zor07 <anzor.karmov@gmail.com>
2022-12-27 10:19:08 +0300

    task content service

--- src/main/java/com/zor07/nofapp/service/levels/TaskContentService.java
+++ src/main/java/com/zor07/nofapp/service/levels/TaskContentService.java
@@ -1,5 +1,9 @@
 package com.zor07.nofapp.service.levels;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.zor07.nofapp.entity.file.File;
+import com.zor07.nofapp.entity.level.Task;
+import com.zor07.nofapp.entity.level.TaskContent;
 import com.zor07.nofapp.repository.level.TaskContentRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,11 +13,24 @@ import javax.transaction.Transactional;
 @Transactional
 public class TaskContentService {
 
-    private final TaskContentRepository taskContentRepository;
+    private final TaskContentRepository repository;
 
-    public TaskContentService(TaskContentRepository taskContentRepository) {
-        this.taskContentRepository = taskContentRepository;
+    public TaskContentService(final TaskContentRepository repository) {
+        this.repository = repository;
     }
 
+    public void saveTaskContent(TaskContent content) {
+        repository.save(content);
+    }
+    public void deleteTaskContent(Long taskContentId) {
+        repository.deleteById(taskContentId);
+    }
 
+    public Task addVideoToTask(Long taskId, File file) {
+        return null;
+    }
+
+    public Task addTextToTask(Long taskId, JsonNode jsonNode) {
+        return null;
+    }
 }
