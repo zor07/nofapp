6ff39e532e3fcd45308c4aef9fa6ed7bbc278bc6
zor07 <anzor.karmov@gmail.com>
2022-12-28 18:57:17 +0300

    taskService field

--- src/main/java/com/zor07/nofapp/api/v1/controller/TaskController.java
+++ src/main/java/com/zor07/nofapp/api/v1/controller/TaskController.java
@@ -1,5 +1,6 @@
 package com.zor07.nofapp.api.v1.controller;
 
+import com.zor07.nofapp.service.levels.TaskService;
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,4 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "Task")
 public class TaskController {
 
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
 }
