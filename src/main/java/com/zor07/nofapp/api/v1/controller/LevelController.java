32bf888916fc7158b1ff3bc953b4528a021045da
zor07 <anzor.karmov@gmail.com>
2022-12-28 18:47:46 +0300

    createLevel endpoint

--- src/main/java/com/zor07/nofapp/api/v1/controller/LevelController.java
+++ src/main/java/com/zor07/nofapp/api/v1/controller/LevelController.java
@@ -11,9 +11,13 @@ import io.swagger.annotations.ApiResponses;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,16 +25,13 @@ import java.util.List;
 @Api(tags = "Level")
 public class LevelController {
 
-
     private final LevelService levelService;
-
     private final LevelMapper levelMapper;
 
     public LevelController(LevelService levelService, LevelMapper levelMapper) {
         this.levelService = levelService;
         this.levelMapper = levelMapper;
     }
-
 
 //    GET    /api/v1/levels  List<LevelDto>
 //    POST   /api/v1/levels create Level
@@ -53,6 +54,26 @@ public class LevelController {
                         .toList()
         );
     }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Creates new level", response = NoteDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successfully created new level"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
+    public ResponseEntity<LevelDto> createLevel(final @RequestBody LevelDto levelDto) {
+        final var level = levelService.save(levelMapper.toEntity(levelDto));
+        final var uri = URI.create(ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .path(String.format("/api/v1/levels/%d", level.getId()))
+                .toUriString());
+        return ResponseEntity.created(uri).body(levelMapper.toDto(level));
+
+    }
+
+
 
 
 
