b0e437332b6e3d986ac8a8343867624f49653a7b
zor07 <anzor.karmov@gmail.com>
2022-12-28 18:42:57 +0300

    getLevels endpoint

--- src/main/java/com/zor07/nofapp/api/v1/controller/LevelController.java
+++ src/main/java/com/zor07/nofapp/api/v1/controller/LevelController.java
@@ -1,10 +1,20 @@
 package com.zor07.nofapp.api.v1.controller;
 
+import com.zor07.nofapp.api.v1.dto.level.LevelDto;
 import com.zor07.nofapp.api.v1.dto.level.mapper.LevelMapper;
+import com.zor07.nofapp.api.v1.dto.notes.NoteDto;
 import com.zor07.nofapp.service.levels.LevelService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/levels")
@@ -27,17 +37,22 @@ public class LevelController {
 //    PUT    /api/v1/levels/{levelId} update level
 //    DELETE /api/v1/levels/{levelId} delete level
 
-//    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-//    @ApiOperation(value = "Gets all levels", response = NoteDto.class)
-//    @ApiResponses(value = {
-//            @ApiResponse(code = 200, message = "Successfully retrieved levels"),
-//            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
-//            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
-//            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
-//    })
-//    public ResponseEntity<List<LevelDto>> getLevels(final @PathVariable Long levelId) {
-//        return ResponseEntity.ok(noteMapper.toDto(noteService.getNote(notebookId, noteId, userId)));
-//    }
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Gets all levels", response = NoteDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved levels"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
+    public ResponseEntity<List<LevelDto>> getLevels() {
+        return ResponseEntity.ok(
+                levelService.getAll()
+                        .stream()
+                        .map(levelMapper::toDto)
+                        .toList()
+        );
+    }
 
 
 
