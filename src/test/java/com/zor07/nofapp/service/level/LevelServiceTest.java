bc5eae5664eb7c784d3e4b1deb743cf9e826e534
zor07 <anzor.karmov@gmail.com>
2022-12-29 23:29:50 +0300

    level service test

---
+++ src/test/java/com/zor07/nofapp/service/level/LevelServiceTest.java
@@ -0,0 +1,63 @@
+package com.zor07.nofapp.service.level;
+
+import com.zor07.nofapp.repository.level.LevelRepository;
+import com.zor07.nofapp.service.levels.LevelService;
+import com.zor07.nofapp.spring.AbstractApplicationTest;
+import com.zor07.nofapp.test.LevelTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LevelServiceTest extends AbstractApplicationTest {
+
+    @Autowired
+    private LevelRepository levelRepository;
+    @Autowired
+    private LevelService levelService;
+
+    @BeforeClass
+    @AfterTest
+    void clearDb() {
+        levelRepository.deleteAll();
+    }
+
+    @Test
+    void saveTest() {
+
+
+    }
+    @Test
+    void getAllTest() {
+
+    }
+    @Test
+    void deleteTest() {
+
+    }
+
+//    @Test
+//    void testCrud() {
+//        levelRepository.deleteAll();
+//        final var all = levelRepository.findAll();
+//        assertThat(all).isEmpty();
+//
+//        final var level = LevelTestUtils.getBlankEntity();
+//
+//        final var id = levelRepository.save(level).getId();
+//        final var inserted = levelRepository.findById(id).get();
+//        LevelTestUtils.checkEntity(level, inserted, false);
+//        LevelTestUtils.updateEntity(inserted);
+//
+//        levelRepository.save(inserted);
+//
+//        final var updated = levelRepository.findById(id).get();
+//        LevelTestUtils.checkUpdated(inserted);
+//
+//        levelRepository.delete(updated);
+//
+//        assertThat(levelRepository.findById(id)).isEmpty();
+//    }
+}
