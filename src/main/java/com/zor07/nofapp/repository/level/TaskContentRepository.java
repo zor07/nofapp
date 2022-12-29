8bd4e8daf392966d8e3766d4300be17fe9af5d0f
zor07 <anzor.karmov@gmail.com>
2022-12-29 20:37:53 +0300

    deleteByTaskId repository

--- src/main/java/com/zor07/nofapp/repository/level/TaskContentRepository.java
+++ src/main/java/com/zor07/nofapp/repository/level/TaskContentRepository.java
@@ -2,8 +2,20 @@ package com.zor07.nofapp.repository.level;
 
 import com.zor07.nofapp.entity.level.TaskContent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
 
 public interface TaskContentRepository extends JpaRepository<TaskContent, Long> {
+
+    @Query(
+        nativeQuery = true,
+        value = "delete from task_content where id in (select task_content_id from task where id = :taskId)"
+    )
+    @Modifying
+    @Transactional
+    void deleteByTaskId(Long taskId);
 }
 
 
