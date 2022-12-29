8e701bf15386b959e8161da203144ecfbeadd134
zor07 <anzor.karmov@gmail.com>
2022-12-29 17:59:38 +0300

    getTasks endpoint

--- src/main/java/com/zor07/nofapp/api/v1/controller/TaskController.java
+++ src/main/java/com/zor07/nofapp/api/v1/controller/TaskController.java
@@ -1,10 +1,21 @@
 package com.zor07.nofapp.api.v1.controller;
 
+import com.zor07.nofapp.api.v1.dto.level.TaskDto;
 import com.zor07.nofapp.api.v1.dto.level.mapper.TaskMapper;
+import com.zor07.nofapp.api.v1.dto.notes.NoteDto;
 import com.zor07.nofapp.service.levels.TaskService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/levels/{levelId}/tasks")
@@ -20,11 +31,25 @@ public class TaskController {
         this.taskMapper = taskMapper;
     }
 
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Gets tasks of level", response = NoteDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved tasks"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
+    public ResponseEntity<List<TaskDto>> getTasks(final @PathVariable Long levelId) {
+        return ResponseEntity.ok(
+                taskService.getAllByLevelId(levelId)
+                        .stream()
+                        .map(taskMapper::toDto)
+                        .toList()
+        );
+    }
 
 
 
-
-//    GET    /api/v1/levels/{levelId}/tasks List<TaskDto>
 //    GET    /api/v1/levels/{levelId}/tasks/{taskId} TaskDto
 //    POST   /api/vi/levels/{levelId}/tasks TaskDto - create task
 //    PUT    /api/vi/levels/{levelId}/tasks/{taskId} TaskDto - update task
