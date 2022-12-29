b9ff6d1e4eddb930470246152e2e80ae6b3fde75
zor07 <anzor.karmov@gmail.com>
2022-12-29 23:13:43 +0300

    fix endpoint

--- src/main/java/com/zor07/nofapp/api/v1/controller/level/TaskController.java
+++ src/main/java/com/zor07/nofapp/api/v1/controller/level/TaskController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -93,7 +94,7 @@ public class TaskController {
         );
     }
 
-    @PostMapping(
+    @PutMapping(
             value = "/{taskId}",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
