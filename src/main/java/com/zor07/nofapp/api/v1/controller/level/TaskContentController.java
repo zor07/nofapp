4af2f0de7b2f8e6496dbea3fe60230009e040037
zor07 <anzor.karmov@gmail.com>
2022-12-29 23:22:17 +0300

    update task content

--- src/main/java/com/zor07/nofapp/api/v1/controller/level/TaskContentController.java
+++ src/main/java/com/zor07/nofapp/api/v1/controller/level/TaskContentController.java
@@ -3,6 +3,7 @@ package com.zor07.nofapp.api.v1.controller.level;
 import com.zor07.nofapp.api.v1.dto.level.TaskContentDto;
 import com.zor07.nofapp.api.v1.dto.level.mapper.TaskContentMapper;
 import com.zor07.nofapp.service.levels.TaskContentService;
+import com.zor07.nofapp.service.levels.TaskService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -12,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +23,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/v1/levels/{levelId}/tasks/{taskId}/content")
@@ -28,11 +31,14 @@ import java.net.URI;
 public class TaskContentController {
 
     private final TaskContentService taskContentService;
+    private final TaskService taskService;
     private final TaskContentMapper taskContentMapper;
 
     public TaskContentController(final TaskContentService taskContentService,
+                                 final TaskService taskService,
                                  final TaskContentMapper taskContentMapper) {
         this.taskContentService = taskContentService;
+        this.taskService = taskService;
         this.taskContentMapper = taskContentMapper;
     }
 
@@ -85,70 +91,29 @@ public class TaskContentController {
         return ResponseEntity.accepted().build();
     }
 
-
-
-
-//    POST   /api/v1/levels/{levelId}/tasks/{taskId}/content/text - upload text to task
-
-//    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-//    @ApiOperation(value = "Gets tasks of level", response = NoteDto.class)
-//    @ApiResponses(value = {
-//            @ApiResponse(code = 200, message = "Successfully retrieved tasks"),
-//            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
-//            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
-//            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
-//    })
-//    public ResponseEntity<List<TaskDto>> getTasks(final @PathVariable Long levelId) {
-//        return ResponseEntity.ok(
-//                taskService.getAllByLevelId(levelId)
-//                        .stream()
-//                        .map(taskMapper::toDto)
-//                        .toList()
-//        );
-//    }
-//
-//
-//
-//    @GetMapping(value = "/{taskId}", produces = MediaType.APPLICATION_JSON_VALUE)
-//    @ApiOperation(value = "Gets tasks of level", response = NoteDto.class)
-//    @ApiResponses(value = {
-//            @ApiResponse(code = 200, message = "Successfully retrieved tasks"),
-//            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
-//            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
-//            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
-//    })
-//    public ResponseEntity<TaskDto> getTask(final @PathVariable Long levelId,
-//                                           final @PathVariable Long taskId) {
-//        return ResponseEntity.ok(
-//            taskMapper.toDto(
-//                taskService.getTask(levelId, taskId)
-//            )
-//        );
-//    }
-//
-//    @PostMapping(
-//            value = "/{taskId}",
-//            consumes = MediaType.APPLICATION_JSON_VALUE,
-//            produces = MediaType.APPLICATION_JSON_VALUE
-//    )
-//    @ApiOperation(value = "Updates given task", response = TaskDto.class)
-//    @ApiResponses(value = {
-//            @ApiResponse(code = 201, message = "Successfully updated task"),
-//            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
-//            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
-//            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
-//    })
-//    public ResponseEntity<TaskDto> updateTask(final @PathVariable Long levelId,
-//                                              final @PathVariable Long taskId,
-//                                              final @RequestBody TaskDto taskDto) {
-//        if (!Objects.equals(taskDto.level().id(), levelId) &&
-//            !Objects.equals(taskDto.id(), taskId)) {
-//            return ResponseEntity.badRequest().build();
-//        }
-//        final var task = taskService.save(taskMapper.toEntity(taskDto));
-//        return ResponseEntity.accepted().body(taskMapper.toDto(task));
-//    }
-//
-
+    @PutMapping(
+            value = "/{taskContentId}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ApiOperation(value = "Updates given task", response = TaskContentDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successfully updated task"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
+    public ResponseEntity<TaskContentDto> updateTaskContent(final @PathVariable Long levelId,
+                                                            final @PathVariable Long taskId,
+                                                            final @PathVariable Long taskContentId,
+                                                            final @RequestBody TaskContentDto taskDto) {
+        final var task = taskService.getTask(levelId, taskId);
+        if (!Objects.equals(taskContentId, taskDto.id()) &&
+            !Objects.equals(task.getId(), taskContentId)) {
+            return ResponseEntity.badRequest().build();
+        }
+        taskContentService.save(taskContentMapper.toEntity(taskDto));
+        return ResponseEntity.accepted().build();
+    }
 
 }
