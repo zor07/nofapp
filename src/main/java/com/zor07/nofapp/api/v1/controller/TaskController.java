d6bd3504b820047a984d4f642c1427510b193370
zor07 <anzor.karmov@gmail.com>
2022-12-29 17:58:36 +0300

    task mapper

--- src/main/java/com/zor07/nofapp/api/v1/controller/TaskController.java
+++ src/main/java/com/zor07/nofapp/api/v1/controller/TaskController.java
@@ -1,5 +1,6 @@
 package com.zor07.nofapp.api.v1.controller;
 
+import com.zor07.nofapp.api.v1.dto.level.mapper.TaskMapper;
 import com.zor07.nofapp.service.levels.TaskService;
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +12,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class TaskController {
 
     private final TaskService taskService;
+    private final TaskMapper taskMapper;
 
-    public TaskController(TaskService taskService) {
+    public TaskController(final TaskService taskService,
+                          final TaskMapper taskMapper) {
         this.taskService = taskService;
+        this.taskMapper = taskMapper;
     }
+
+
+
+
+
+//    GET    /api/v1/levels/{levelId}/tasks List<TaskDto>
+//    GET    /api/v1/levels/{levelId}/tasks/{taskId} TaskDto
+//    POST   /api/vi/levels/{levelId}/tasks TaskDto - create task
+//    PUT    /api/vi/levels/{levelId}/tasks/{taskId} TaskDto - update task
+//    DELETE /api/vi/levels/{levelId}/tasks/{taskId} - delete task
+//
+//    TaskService
+//    getAllByLevelId(Long levelId)
+//    save(Task task)
+//    delete(Long id)
+//    deleteTaskContent(Long taskId)
+//    createTaskContent(TaskContent content)
+//    addVideoToTask(taskId, file)
+//    addTextToTask(taskId, jsonNode)
 }
