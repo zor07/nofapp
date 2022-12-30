f0dc9c1e145628b21706e6f9560d817a56736f98
zor07 <anzor.karmov@gmail.com>
2022-12-30 16:47:02 +0300

    LevelControllerTest update

--- src/test/java/com/zor07/nofapp/api/v1/controller/level/LevelControllerTest.java
+++ src/test/java/com/zor07/nofapp/api/v1/controller/level/LevelControllerTest.java
@@ -6,9 +6,7 @@ import com.zor07.nofapp.test.LevelTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -23,10 +21,6 @@ public class LevelControllerTest extends AbstractApiTest {
 
     @Autowired
     private LevelRepository levelRepository;
-    @Autowired
-    private WebApplicationContext context;
-    private MockMvc mvc;
-
     private static final String LEVELS_ENDPOINT  = "/api/v1/levels";
     private static final String LEVEL_ENDPOINT  = LEVELS_ENDPOINT + "/%s";
 
