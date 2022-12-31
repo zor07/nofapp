1aec0676b695081694e34bc3b42e6ee9dbcdfd61
zor07 <anzor.karmov@gmail.com>
2022-12-31 14:44:00 +0300

    Autowired

--- src/test/java/com/zor07/nofapp/api/v1/controller/level/LevelControllerTest.java
+++ src/test/java/com/zor07/nofapp/api/v1/controller/level/LevelControllerTest.java
@@ -19,15 +19,17 @@ import java.util.List;
 import static com.zor07.nofapp.test.UserTestUtils.DEFAULT_USERNAME;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class LevelControllerTest extends AbstractApiTest {
 
-    @Autowired
-    private LevelRepository levelRepository;
-    @Autowired
-    private LevelMapper levelMapper;
+
+    private @Autowired LevelRepository levelRepository;
+    private @Autowired LevelMapper levelMapper;
     private static final String LEVELS_ENDPOINT  = "/api/v1/levels";
     private static final String LEVEL_ENDPOINT  = LEVELS_ENDPOINT + "/%s";
 
