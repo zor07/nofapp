cea6297dbb1f31f2bc3fef58632083507e199d32
zor07 <anzor.karmov@gmail.com>
2022-12-31 14:41:19 +0300

    removed field

--- src/test/java/com/zor07/nofapp/api/v1/controller/NoteControllerTest.java
+++ src/test/java/com/zor07/nofapp/api/v1/controller/NoteControllerTest.java
@@ -7,7 +7,6 @@ import com.zor07.nofapp.entity.notes.Note;
 import com.zor07.nofapp.entity.notes.Notebook;
 import com.zor07.nofapp.repository.notes.NoteRepository;
 import com.zor07.nofapp.repository.notes.NotebookRepository;
-import com.zor07.nofapp.repository.user.UserRepository;
 import com.zor07.nofapp.spring.AbstractApiTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -42,8 +41,6 @@ public class NoteControllerTest extends AbstractApiTest {
   private NotebookRepository notebookRepository;
   @Autowired
   private NoteRepository noteRepository;
-  @Autowired
-  private UserRepository userRepository;
 
   private Notebook createNotebook(final String username) {
     final var user = userService.getUser(username);
