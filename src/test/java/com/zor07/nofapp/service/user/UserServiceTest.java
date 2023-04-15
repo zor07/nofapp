a4900ea01a82d9d06d7543062d12a5efcc67b1fe
zor07 <anzor.karmov@gmail.com>
2023-04-15 17:11:14 +0300

    tabulation

--- src/test/java/com/zor07/nofapp/service/user/UserServiceTest.java
+++ src/test/java/com/zor07/nofapp/service/user/UserServiceTest.java
@@ -2,7 +2,6 @@ package com.zor07.nofapp.service.user;
 
 import com.zor07.nofapp.repository.user.RoleRepository;
 import com.zor07.nofapp.repository.user.UserRepository;
-import com.zor07.nofapp.service.user.UserService;
 import com.zor07.nofapp.spring.AbstractApplicationTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -12,83 +11,86 @@ import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 
-import static com.zor07.nofapp.test.UserTestUtils.*;
+import static com.zor07.nofapp.test.UserTestUtils.DEFAULT_ROLE;
+import static com.zor07.nofapp.test.UserTestUtils.DEFAULT_USERNAME;
+import static com.zor07.nofapp.test.UserTestUtils.createRole;
+import static com.zor07.nofapp.test.UserTestUtils.createUser;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserServiceTest extends AbstractApplicationTest {
 
-  @Autowired
-  private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-  @Autowired
-  private RoleRepository roleRepository;
+    @Autowired
+    private RoleRepository roleRepository;
 
-  @Autowired
-  protected UserService userService;
+    @Autowired
+    protected UserService userService;
 
-  private void clearDb() {
-    userRepository.deleteAll();
-    roleRepository.deleteAll();
-  }
+    private void clearDb() {
+        userRepository.deleteAll();
+        roleRepository.deleteAll();
+    }
 
-  @BeforeMethod
-  void setup() {
-    clearDb();
-  }
+    @BeforeMethod
+    void setup() {
+        clearDb();
+    }
 
-  @AfterClass
-  void teardown() {
-    clearDb();
-  }
+    @AfterClass
+    void teardown() {
+        clearDb();
+    }
 
-  @Test
-  void saveUserTest() {
-    userService.saveUser(createUser());
-    final var user = userRepository.findByUsername(DEFAULT_USERNAME);
-    assertThat(user.getName()).isEqualTo(DEFAULT_USERNAME);
-  }
+    @Test
+    void saveUserTest() {
+        userService.saveUser(createUser());
+        final var user = userRepository.findByUsername(DEFAULT_USERNAME);
+        assertThat(user.getName()).isEqualTo(DEFAULT_USERNAME);
+    }
 
-  @Test(expectedExceptions = DataIntegrityViolationException.class)
-  void saveUserTest_failsWhenSavingUsersWithSameUsername() {
-    userService.saveUser(createUser());
-    userService.saveUser(createUser());
-  }
+    @Test(expectedExceptions = DataIntegrityViolationException.class)
+    void saveUserTest_failsWhenSavingUsersWithSameUsername() {
+        userService.saveUser(createUser());
+        userService.saveUser(createUser());
+    }
 
-  @Test
-  void saveRoleTest() {
-    userService.saveRole(createRole());
-    final var user = roleRepository.findAll().get(0);
-    assertThat(user.getName()).isEqualTo(DEFAULT_ROLE);
-  }
+    @Test
+    void saveRoleTest() {
+        userService.saveRole(createRole());
+        final var user = roleRepository.findAll().get(0);
+        assertThat(user.getName()).isEqualTo(DEFAULT_ROLE);
+    }
 
-  @Test(expectedExceptions = DataIntegrityViolationException.class)
-  void saveRoleTest_failsWhenSavingRolesWithSameName() {
-    userService.saveRole(createRole());
-    userService.saveRole(createRole());
-  }
+    @Test(expectedExceptions = DataIntegrityViolationException.class)
+    void saveRoleTest_failsWhenSavingRolesWithSameName() {
+        userService.saveRole(createRole());
+        userService.saveRole(createRole());
+    }
 
-  @Test
-  void addRoleToUserTest() {
-    userService.saveUser(createUser());
-    userService.saveRole(createRole());
-    userService.addRoleToUser(DEFAULT_USERNAME, DEFAULT_ROLE);
-    final var user = userRepository.findByUsername(DEFAULT_USERNAME);
-    final var roles = new ArrayList<>(user.getRoles());
-    assertThat(roles).hasSize(1);
-    assertThat(roles.get(0).getName()).isEqualTo(DEFAULT_ROLE);
-  }
+    @Test
+    void addRoleToUserTest() {
+        userService.saveUser(createUser());
+        userService.saveRole(createRole());
+        userService.addRoleToUser(DEFAULT_USERNAME, DEFAULT_ROLE);
+        final var user = userRepository.findByUsername(DEFAULT_USERNAME);
+        final var roles = new ArrayList<>(user.getRoles());
+        assertThat(roles).hasSize(1);
+        assertThat(roles.get(0).getName()).isEqualTo(DEFAULT_ROLE);
+    }
 
-  @Test
-  void getUserTest() {
-    saveUserTest();
-  }
+    @Test
+    void getUserTest() {
+        saveUserTest();
+    }
 
-  @Test
-  void getUsersTest() {
-    userService.saveUser(createUser("user1"));
-    userService.saveUser(createUser("user2"));
-    userService.saveUser(createUser("user3"));
-    assertThat(userService.getUsers()).hasSize(3);
-  }
+    @Test
+    void getUsersTest() {
+        userService.saveUser(createUser("user1"));
+        userService.saveUser(createUser("user2"));
+        userService.saveUser(createUser("user3"));
+        assertThat(userService.getUsers()).hasSize(3);
+    }
 
 }
